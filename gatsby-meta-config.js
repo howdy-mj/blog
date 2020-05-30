16852f3d63a89c2ab704bd100e29e8b790dabcea
howdy-mj <hi.minjungkim@gmail.com>
2020-05-30 14:18:42 +0900

    imges

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: `MJ Kim`,
-  description: `Blog posted about ...`,
+  description: `공부한 것을 기록하는 블로그`,
   author: `김민정(howdy-mj)`,
   introduction: `간결한 코드를 작성하려 노력하는 프론트엔드 개발자 김민정입니다. 회고록, 일상 및 다른 관심사는 티스토리에 기록하고 있습니다.`,
   siteUrl: `https://howdy-mj.netlify.app/`,
@@ -11,7 +11,7 @@ module.exports = {
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/lego_icon.png`,
   keywords: [`frontend`, `react`, `javascript`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
@@ -21,7 +21,7 @@ module.exports = {
     countOfInitialPost: 10,
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'eDQ9SyAo0',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
