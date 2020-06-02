bb26503a266d2c91547564e64569d8a92942ef9e
mj <hi.minjungkim@gmail.com>
2020-06-02 11:10:58 +0900

    fix repo

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -15,7 +15,7 @@ module.exports = {
   keywords: [`frontend`, `react`, `javascript`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'howdy-mj/mj-blog', // Your repository for archive comment
+    utterances: 'howdy-mj/blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10,
