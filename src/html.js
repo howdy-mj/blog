52e66972509934b4dd804776c90e94b29b3c1e5d
howdy-mj <hi.minjungkim@gmail.com>
2021-08-02 19:26:04 +0900

    chore: google search console 추가

--- src/html.js
+++ src/html.js
@@ -13,6 +13,10 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=2"
           />
           {this.props.headComponents}
+          <meta
+            name="google-site-verification"
+            content="8OdXHKUJ55_TUQ39BmhmYR3HnPf2qQZ3_E10IfzC9jU"
+          />
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
